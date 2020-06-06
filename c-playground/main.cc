dee327889398c248127314f5a0404f0d72f74caa
Ichisada Shioko <ichisadashioko@gmail.com>
2020-06-06 13:33:14 +0900

    free memory with delete[]
    - delete[] will recursively free pointers
    - delete[] will not free pointers if they were still referenced
    at the time of delete[] call

--- c-playground/main.cc
+++ c-playground/main.cc
@@ -12,17 +12,18 @@ int main() {
   unsigned int numIterations = 32000;
   STRUCT_WITH_POINTER_A* x = NULL;
 
-  for (unsigned int i = 0; i < numIterations; i++) {
-    if (x != NULL) {
-      free(x[0].CharArray);
-      free(x[0].IntArray);
-    }
+  int* lastIntArray = NULL;
+  char* lastCharArray = NULL;
 
-    free(x);
+  for (unsigned int i = 0; i < numIterations; i++) {
+    delete[] x;
     x = (STRUCT_WITH_POINTER_A*)malloc(sizeof(STRUCT_WITH_POINTER_A));
 
-    x[0].IntArray = (int*)malloc(sizeof(int) * numElements);
-    x[0].CharArray = (char*)malloc(sizeof(char) * numElements);
+    lastIntArray = (int*)malloc(sizeof(int) * numElements);
+    x[0].IntArray = lastIntArray;
+
+    lastCharArray = (char*)malloc(sizeof(char) * numElements);
+    x[0].CharArray = lastCharArray;
   }
 
   return 0;
