48b9476af1d71bbac7371d0a6dcca40c8f05467d
Ichisada Shioko <ichisadashioko@gmail.com>
2020-06-06 09:52:26 +0900

    leaking memory example

--- c-playground/main.cc
+++ c-playground/main.cc
@@ -1,2 +1,21 @@
 #include <stdio.h>
+
 #include <iostream>
+
+struct STRUCT_WITH_POINTER_A {
+  int* IntArray = NULL;
+  char* CharArray = NULL;
+};
+
+int main() {
+  unsigned int numElements = 10000;
+  unsigned int numIterations = 32000;
+  STRUCT_WITH_POINTER_A x;
+
+  for (unsigned int i = 0; i < numIterations; i++) {
+    x.IntArray = (int*)malloc(sizeof(int) * numElements);
+    x.CharArray = (char*)malloc(sizeof(char) * numElements);
+  }
+
+  return 0;
+}
